50ece1bbe4627709d8facc7b74402bbb6a49e32f
bsudekum <bobby@mapbox.com>
2016-09-02 14:58:35 -0700

    Provide a default instruction

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
 var instructions = require('./instructions.json');
 
 // Assertions
-if (Object !== instructions.constructor) throw "instructions must be object";
-
-var validLevels = [ 'high' ];
+if (Object !== instructions.constructor) throw 'instructions must be object';
 
 module.exports = {
-    compile: function(step, version, level, cb) {
+    compile: function(step, version, cb) {
         // TODO: Validations
 
-        var err;
         var type = step.maneuver.type;
         var modifier = step.maneuver.modifier;
 
-        if(!instructions[version]) { return cb(new Error("Invalid version")); }
-        if(!type) { return cb(new Error("Missing step maneuver type")); }
-        if(!modifier) { return cb(new Error('Missing step maneuver modifier')); }
-        if(validLevels.indexOf(level) < 0) { return cb(new Error('Invalid level ' + level)); }
+        if (!instructions[version]) { return cb(new Error('Invalid version')); }
+        if (!type) { return cb(new Error('Missing step maneuver type')); }
+        if (!modifier) { return cb(new Error('Missing step maneuver modifier')); }
+        if (!instructions[version][type]) return cb(new Error('Unknown type', type));
+        if (!step.maneuver.modifier) return cb(new Error('No maneuver provided'));
 
-        if(!instructions[version][type]) {
-          // if we have an unknown type, then use `turn` as default
-          type = 'turn';
-        }
-        if(!instructions[version][type][modifier]) {
-          return cb(new Error('Invalid step maneuver type-modifier combination ' + type + ' ' + modifier));
-        }
 
-        var template = instructions[version][type][modifier][level];
+        // First check if the modifier for this maneuver has a special instruction
+        // If not, use the `defaultInstruction`
+        var template = instructions[version][type][modifier]
+            ? instructions[version][type][modifier] : instructions[version][type].defaultInstruction;
 
         if (step.name && step.name !== '') {
             return cb(
                 null,
                 template
-                  .replace('[', '')
-                  .replace(']', '')
-                  .replace('{way_name}', step.name)
-                  .trim()
+                    .replace('[', '')
+                    .replace(']', '')
+                    .replace('{way_name}', step.name)
+                    .replace('{modifier}', step.maneuver.modifier)
+                    .trim()
             );
         } else {
             // remove name
             return cb(
                 null,
                 template
-                  .replace(/\[.+\]/, '')
-                  .trim()
+                    .replace(/\[.+\]/, '')
+                    .replace('{modifier}', step.maneuver.modifier)
+                    .trim()
             );
         }
     }
