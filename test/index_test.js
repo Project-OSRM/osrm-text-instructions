fd6e012d7f2695e84931c7b1e7a52775310662da
bsudekum <bobby@mapbox.com>
2016-09-07 11:02:55 -0700

    add all the tests

--- test/index_test.js
+++ test/index_test.js
@@ -1,16 +1,39 @@
 var fs = require('fs');
 var tape = require('tape');
+var path = require('path');
+var instructions = require('../index.js');
+var constants = require('../lib/constants');
 
 tape.test('v5 compile', function(t) {
-    var instructions = require('../index.js')('v5');
+    var v5Instructions = instructions('v5');
 
-    t.test('from fixtures', (assert) => {
-        var d = __dirname + '/fixtures/v5/';
-        fs.readdirSync(d).forEach((f) => {
-            if (f.match(/json$/)) {
-                var fixture = JSON.parse(fs.readFileSync(d + f))
-                assert.equal(instructions.compile(fixture.step), fixture.instruction, fixture.testName)
-            }
+    t.test('from fixtures', function(assert) {
+        var d = path.join(__dirname, 'fixtures', 'v5/');
+
+        fs.readdirSync(d).forEach(function(folder) {
+            if (folder === '.DS_Store') return;
+            fs.readdirSync(d + folder).forEach(function(p) {
+                if (p.match(/json$/)) {
+                    var fixture = JSON.parse(fs.readFileSync(path.join(d, folder, p)));
+
+                    assert.equal(v5Instructions.compile(fixture.step), fixture.instruction, fixture.testName);
+                }
+            });
+        });
+
+        assert.end();
+    });
+
+    t.test('fixture exists for every type/modifier combinations', function(assert) {
+        var basePath = path.join(__dirname, 'fixtures', 'v5/');
+
+        fs.readdirSync(basePath).forEach(function(type) {
+            if (type === '.DS_Store') return;
+            fs.readdirSync(basePath + type).forEach(function() {
+                constants.modifiers.forEach(function(modifier) {
+                    assert.ok(fs.existsSync(path.join(basePath, type, modifier.replace(' ', '_') + '.json')), type + '/' + modifier);
+                });
+            });
         });
 
         assert.end();
