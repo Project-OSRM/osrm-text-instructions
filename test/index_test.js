6bb183c7d897aeadfda65261ba6e191d61f4a74c
Freenerd <nospam@freenerd.de>
2016-09-08 16:03:14 +0200

    Tests: Check for no_name fixture files

--- test/index_test.js
+++ test/index_test.js
@@ -7,38 +7,49 @@ var constants = require('../lib/constants');
 tape.test('v5 compile', function(t) {
     var v5Instructions = instructions('v5');
 
-    t.test('from fixtures', function(assert) {
-        var d = path.join(__dirname, 'fixtures', 'v5/');
+    t.test('fixtures exist for every type/modifier combinations', function(assert) {
+        var instructions = require('../instructions');
+        var basePath = path.join(__dirname, 'fixtures', 'v5/');
 
-        fs.readdirSync(d).forEach(function(folder) {
-            if (folder === '.DS_Store') return;
-            fs.readdirSync(d + folder).forEach(function(p) {
-                if (p.match(/json$/)) {
-                    var b = path.join(d, folder, p);
-                    var fixture = JSON.parse(fs.readFileSync(b));
+        constants.types.forEach(function(type) {
+            constants.modifiers.forEach(function(modifier) {
+                // check normal fixture
+                var p = path.join(basePath, type, modifier.replace(' ', '_') + '.json');
+                 assert.ok(fs.existsSync(p), type + '/' + modifier);
 
-                    if (process.env.UPDATE) {
-                        fixture.instruction = v5Instructions.compile(fixture.step);
-                        fs.writeFileSync(b, JSON.stringify(fixture, null, 4));
-                    } else {
-                        assert.equal(v5Instructions.compile(fixture.step), fixture.instruction, fixture.testName);
-                    }
-                }
+                // check no_name fixture if should exist
+                var noNamePath = path.join(basePath, type, modifier.replace(' ', '_') + '_no_name.json');
+                if (instructions['v5'][type.replace(/_/g, ' ')]['defaultInstruction'].match('way_name')) {
+                     assert.ok(fs.existsSync(noNamePath), type + '/' + modifier + '/no_name');
+                };
             });
         });
 
         assert.end();
     });
 
-    t.test('fixture exists for every type/modifier combinations', function(assert) {
+    t.test('fixtures match generated instructions', function(assert) {
         var basePath = path.join(__dirname, 'fixtures', 'v5/');
 
         fs.readdirSync(basePath).forEach(function(type) {
-            if (type === '.DS_Store' || type === 'other') return;
-            fs.readdirSync(basePath + type).forEach(function() {
-                constants.modifiers.forEach(function(modifier) {
-                    assert.ok(fs.existsSync(path.join(basePath, type, modifier.replace(' ', '_') + '.json')), type + '/' + modifier);
-                });
+            if (type === '.DS_Store') return;
+
+            fs.readdirSync(basePath + type).forEach(function(file) {
+                if (!file.match(/json$/)) return;
+
+                var p = path.join(basePath, type, file);
+                var fixture = JSON.parse(fs.readFileSync(p));
+
+                if (process.env.UPDATE) {
+                    fixture.instruction = v5Instructions.compile(fixture.step);
+                    fs.writeFileSync(p, JSON.stringify(fixture, null, 4));
+                } else {
+                    assert.equal(
+                        v5Instructions.compile(fixture.step),
+                        fixture.instruction,
+                        type + '/' + file
+                    );
+                }
             });
         });
 
