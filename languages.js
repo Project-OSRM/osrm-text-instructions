f905cba312fafd6e223262a243ac8fa471f629b2
Minh Nguyễn <mxn@1ec5.org>
2017-04-02 00:17:59 -0700

    Spanish from Transifex

--- languages.js
+++ languages.js
@@ -2,6 +2,7 @@
 // with bundling tools like webpack and browserify
 var instructionsDe = require('./languages/translations/de.json');
 var instructionsEn = require('./languages/translations/en.json');
+var instructionsEs = require('./languages/translations/es.json');
 var instructionsFr = require('./languages/translations/fr.json');
 var instructionsNl = require('./languages/translations/nl.json');
 var instructionsRu = require('./languages/translations/ru.json');
@@ -13,6 +14,7 @@ var instructionsZhHans = require('./languages/translations/zh-Hans.json');
 var tags = {
     'de': instructionsDe,
     'en': instructionsEn,
+    'es': instructionsEs,
     'fr': instructionsFr,
     'nl': instructionsNl,
     'ru': instructionsRu,
