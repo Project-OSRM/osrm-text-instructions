b0c2a788a7e5172ae1df2c452019d2ca8f753621
Minh Nguyễn <mxn@1ec5.org>
2017-05-04 12:31:59 -0700

    Added Indonesian localization from Transifex

--- languages.js
+++ languages.js
@@ -4,6 +4,7 @@ var instructionsDe = require('./languages/translations/de.json');
 var instructionsEn = require('./languages/translations/en.json');
 var instructionsEs = require('./languages/translations/es.json');
 var instructionsFr = require('./languages/translations/fr.json');
+var instructionsId = require('./languages/translations/id.json');
 var instructionsNl = require('./languages/translations/nl.json');
 var instructionsRu = require('./languages/translations/ru.json');
 var instructionsSv = require('./languages/translations/sv.json');
@@ -16,6 +17,7 @@ var tags = {
     'en': instructionsEn,
     'es': instructionsEs,
     'fr': instructionsFr,
+    'id': instructionsId,
     'nl': instructionsNl,
     'ru': instructionsRu,
     'sv': instructionsSv,
