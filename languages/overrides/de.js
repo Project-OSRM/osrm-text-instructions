d43cc6f11079aef5ed19658ad1c3e56a1213eedc
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-09-14 12:22:56 -0400

    one more attempt to pull in updates

--- languages/overrides/de.js
+++ languages/overrides/de.js
@@ -1,8 +1,5 @@
 module.exports = function(content) {
     content.meta.capitalizeFirstLetter = true;
 
-    // No special handling needed in German for this one
-    delete content.v5.continue.straight;
-
     return content;
 };
